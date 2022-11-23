a9496a712a81b755646add611314aca59dc07244
Amitavpanda <pandaamitav01@gmail.com>
2022-11-23 17:08:37 +0530

    complete the banner section including search component

--- src/banner.jsx
+++ src/banner.jsx
@@ -1,10 +1,30 @@
-import React from "react";
+import { Button } from "@mui/material";
+import React, { useState } from "react";
+import "./banner.css";
+import Search from "./search";
 
 const Banner = () => {
+    const [showSearch, setShowSearch] = useState(false);
+
     return ( <
         div className = "banner" >
         <
-        h1 > I am banner < /h1> <
+        div className = "banner_search" > { showSearch && < Search / > } <
+        Button onClick = {
+            () => setShowSearch(!showSearch) }
+        className = "banner_searchButton"
+        variant = "contained" >
+        { showSearch ? "Hide" : "Search Dates" } <
+        /Button> <
+        /div> <
+        div className = "banner_info" >
+        <
+        h1 > Get out and stretch your imagination < /h1> <
+        h5 >
+        Plan a different kind of getaway to uncover the hidden gems near you. <
+        /h5> <
+        Button variant = "contained" > Explore nearby < /Button> <
+        /div> <
         /div>
     );
 };
--- src/home.jsx
+++ src/home.jsx
@@ -4,7 +4,6 @@ const Home = () => {
     return ( <
         div >
         <
-        h1 > Home < /h1> <
         Banner / >
         <
         /div>
