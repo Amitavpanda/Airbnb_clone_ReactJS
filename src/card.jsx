02e0962d742b9dc455ff08bbf6c16b6f761e245d
Amitavpanda <pandaamitav01@gmail.com>
2022-11-25 12:23:02 +0530

    add card section

---
+++ src/card.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./card.css";
+const Card = ({ src, title, description, price }) => {
+    return ( <
+        div className = "card" >
+        <
+        img src = { src }
+        alt = "not found" / >
+        <
+        div className = "card_info" >
+        <
+        h2 > { title } < /h2> <
+        h4 > { description } < /h4> <
+        h3 > { price } < /h3> <
+        /div> <
+        /div>
+    );
+};
+
+export default Card;
