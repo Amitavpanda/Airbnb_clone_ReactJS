e371c6cf8a2addb0c415f41a46677f4caaad7667
Amitavpanda <pandaamitav01@gmail.com>
2022-11-25 12:44:49 +0530

    add footer section

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import "./App.css";
 import Header from "./header";
 import Home from "./home";
+import Footer from "./footer";
+
 function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Home />
-    </div>
-  );
+    return ( <
+        div className = "App" >
+        <
+        Header / >
+        <
+        Home / >
+        <
+        Footer / >
+        <
+        /div>
+    );
 }
 
 export default App;
